b3a8e02260282915c4a060a7bdaec6c7c9ebaf02
Ishan-Mulani <89037495+Ishan-Mulani@users.noreply.github.com>
2022-08-02 22:13:30 +0530

    update: url slug

--- routes/articles.js
+++ routes/articles.js
@@ -9,16 +9,17 @@ router.get("/new", (req, res)=>{
     res.render('new', {article: new Article() })
 })
  
-router.get("/:id/edit", async(req, res)=>{
-    const {id} = req.params
-    const article = await Article.findById(id)
+// Get request for edit
+router.get("/:slug/edit", async(req, res)=>{
+    const {slug} = req.params
+    const article = await Article.findOne({slug: slug})
     res.render("edit", {article})
 })
 
 // Get request for detailed blog / showing
-router.get("/:id", async(req, res)=>{
-    const {id} = req.params
-    const article = await Article.findById(id)
+router.get("/:slug", async(req, res)=>{
+    const {slug} = req.params
+    const article = await Article.findOne({slug: slug})  
     res.render("show", {article})
 })
 
@@ -27,19 +28,20 @@ router.post("/", async(req, res)=>{
     console.log(req.body)
     const newArticle = new Article(req.body)
     await newArticle.save()
-    res.redirect(`/articles/${newArticle._id}`)
+    res.redirect(`/articles/${newArticle.slug}`)
  })
 
 //  Put request for editing blog
-router.put("/:id", async(req, res)=>{
-    const {id} = req.params
-    const article = await Article.findByIdAndUpdate(id, req.body, {runValidators:true, new:true})
-    res.redirect(`/articles/${article._id}`)
+router.put("/:slug", async(req, res)=>{
+    const {slug} = req.params
+    const article = await Article.findOneAndUpdate({slug: slug}, req.body, {runValidators:true, new:true}) 
+    // might be troublesome
+    res.redirect(`/articles/${article.slug}`)
 })
 
-router.delete("/:id", async(req, res)=>{
-    const {id} = req.params
-    const deletedArticle = await Article.findByIdAndDelete(id)
+router.delete("/:slug", async(req, res)=>{
+    const {slug} = req.params
+    const deletedArticle = await Article.findOneAndDelete({slug: slug})
     res.redirect("/")
 })
 
